369af895f1cb376e9bd10c13574d56335850d0f6
HyoBin Park <hyobin0726@naver.com>
2023-07-31 21:35:09 +0900

    미리보기와 요약문 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
@@ -17,3 +18,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_ext(self):
+        return self.get_file_name().splite('.')[-1]
