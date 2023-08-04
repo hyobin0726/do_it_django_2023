4ee5702aa7a77ae510a16983cda82dfee415461c
HyoBin Park <hyobin0726@naver.com>
2023-08-04 14:20:05 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -103,27 +103,26 @@ class TestView(TestCase):
         self.assertIn('아직 게시물이 없습니다', main_area.text)
 
     def test_post_detail(self):
-        post_001 = Post.objects.create(
-            title='첫번째 포스트입니다.',
-            content='Hello World. We are the world.',
-            author=self.user_ahyobin
-        )
 
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
 
-        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
+
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         self.navbar_test(soup)
 
-        self.assertIn(post_001.title, soup.title.text)
+        self.category_card_test(soup)
+
+        self.assertIn(self.post_001.title, soup.title.text)
 
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_cafe.name, post_area.text)
 
         # 1.5  첫 번째 post의 작성자(author)가 post-area에 있다.
         # 아직 작성 불가
         self.assertIn(self.user_ahyobin.username.upper(), post_area.text)
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
